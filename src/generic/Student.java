fc705e21ca3dbb4334d6ad089baebdad11c19d11
Vaibhav Diwan <vaibhav.diwan@niit.com>
2023-04-19 11:29:07 +0530

    added one more type parameter into Student class

--- src/generic/Student.java
+++ src/generic/Student.java
@@ -1,24 +1,26 @@
 package generic;
 
-public class Student<T> {
+public class Student<T,E> {
     private T rollNo;
-    private String name;
+
+    //name either of type String or of type Name class
+    private E name;
     //getter and setter
 
-    public String getName() {
+    public E getName() {
         return name;
     }
     public T getRollNo(){
         return rollNo;
     }
 
-    public void setName(String name) {
+    public void setName(E name) {
         this.name = name;
     }
     public void setRollNo(T rollNo){
         this.rollNo = rollNo;
     }
-    public Student(T rollNo, String name){
+    public Student(T rollNo, E name){
         this.rollNo = rollNo;
         this.name = name;
     }
